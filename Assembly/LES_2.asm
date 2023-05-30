d7b64a5f60a82454d3fdea6b05bf50d4340268c5
Christos Katsanis <xristos19972195@gmail.com>
2023-05-30 15:52:54 +0300

    Update LES_2.asm

--- Assembly/LES_2.asm
+++ Assembly/LES_2.asm
@@ -17,31 +17,33 @@ begin:
     mov cl, 0
     
     start:        
-        lea dx, msg1
-        mov ah, 9
-        int 21h        ; output string at ds:dx
-        
-        mov ah, 08h    ; eisagwgh apo pliktrologio me 8h klhsh tou dos
-        int 21h
-                   
-        add al, 48     
-        cmp al, '0'     ; elenxos an einai mikroteros tou 0                          
-        jbe start      
-        
-        cmp al, '9'     ; elenxos an einai megalyteros tou 9
-        jae start
-        
-        mov dl, al     ; emfanish arithmoy 
-        mov ah, 2
-        int 21h
-        
-        cmp al, '0'
-        je m2
-        
-        m2: 
+       lea dx, msg1
+       mov ah, 9
+       int 21h        ; output string at ds:dx
+             
+       mov al, 0              
+       mov ah, 08h    ; eisagwgh apo pliktrologio me 8h klhsh tou dos
+       int 21h
+                        
+       add al, 48     
+       cmp al, '0'     ; elenxos an einai mikroteros tou 0                          
+       jbe start      
+       
+       cmp al, '9'     ; elenxos an einai megalyteros tou 9
+       jae start
+       
+       mov dl, al     ; emfanish arithmoy 
+       mov ah, 2                           
+       int 21h
+       
+       cmp al, '0'
+       je m2
+       
+       m2: 
             lea dx, msg2
             mov ah, 9
             int 21h
+            jmp next
        
        mov ah, 0 
        mov bl, 2
@@ -54,17 +56,20 @@ begin:
             lea dx, msg3
             mov ah, 9
             int 21h
+            jmp next
             
        jne m4
         
        m4:
             lea dx, msg4
             mov ah, 9
-            int 21h
-       
-       inc cl
-       cmp cl, 5
-       je start 
+            int 21h  
+            jmp next
+            
+       next:
+           inc cl
+           cmp cl, 5
+           jne start 
         
     mov ax, 4c00h ; exit to operating system.
     int 21h    
